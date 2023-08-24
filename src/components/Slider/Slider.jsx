1dbaaf2ca6a8af357fd2383687c536b1baa1fdc2
Kabol Amine <amine.kabol@gmail.com>
2023-08-24 16:17:44 +0200

    implementation de la function toggleImage

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,16 +1,42 @@
+import { useState, useEffect } from "react"
 import leftArrow from "../../assets/left-arrow.svg"
 import rightArrow from "../../assets/right-arrow.svg"
 import "./Slider.css"
+import sliderData from "../../data/sliderData.js"
 
 export default function Slider() {
+  const [sliderIndex, setSliderIndex] = useState(1)
+
+  function toggleImage(indexPayload) {
+    let newState
+    if (indexPayload + sliderIndex > sliderData.length) {
+      newState = 1
+    }
+    else if (indexPayload + sliderIndex < 1) {
+      newState = sliderData.length
+    }
+    else {
+      newState = indexPayload + sliderIndex
+    }
+    setSliderIndex(newState)
+  }
+
+  useEffect(() => {
+    const intervalID = setInterval(() => toggleImage(1), 2000);
+  }, [])
+
   return (
     <>
-      <p className="index-info"> 3 / 5</p>
+      <p className="index-info"> {sliderIndex} / {sliderData.length} </p>
       <div className="slider">
-        <p className="image-info">Bedrom</p>
-        <img src="/images/img-3.jpg" alt="" className="slider-img" />
-        <button className="navigation-button prev-button"><img src={leftArrow} alt="" /></button>
-        <button className="navigation-button next-button"><img src={rightArrow} alt="" /></button>
+        <p className="image-info">{sliderData.find(obj => obj.id === sliderIndex).description}</p>
+        <img src={`/images/img-${sliderIndex}.jpg`} alt="" className="slider-img" />
+        <button
+          onClick={() => toggleImage(-1)}
+          className="navigation-button prev-button"><img src={leftArrow} alt="" /></button>
+        <button
+          onClick={() => toggleImage(1)}
+          className="navigation-button next-button"><img src={rightArrow} alt="" /></button>
       </div>
 
     </>
